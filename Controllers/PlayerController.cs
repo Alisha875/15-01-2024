e54e47e8d0d2ae423550a5ffc433e55a11ba6816
Alisha875 <alishanayak001@gmail.com>
2024-01-15 15:23:13 +0530

    Assignment-7

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using Assignments.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignments.Controllers
+{
+    public class PlayerController : Controller
+    {
+        static List<Player> players = new List<Player>()
+        {
+            new Player() {PId = 1, PName="Dhoni",PCountry="India",PType="All-Rounder"},
+            new Player() {PId = 2, PName="Kohli",PCountry="India",PType="Batsman"},
+            new Player() {PId = 3, PName="Rohit",PCountry="India",PType="Batsman"},
+            new Player() {PId = 4, PName="Shubman",PCountry="India",PType="Batsman"},
+            new Player() {PId = 5, PName="Sachin",PCountry="India",PType="Bowler"},
+
+        };
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+        public IActionResult Create(Player play)
+        {
+            if (ModelState.IsValid)
+            {
+                players.Add(play);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
